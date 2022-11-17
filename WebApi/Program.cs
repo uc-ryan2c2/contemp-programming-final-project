dca97941e406c018468e7a19bfcc5883ea959a91
ajwhite00 <aaronjwhite00@gmail.com>
2022-11-17 13:03:21 -0500

    New Dependencies and Versions

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.EntityFrameworkCore;
-using WebApiDemo.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ToDoContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoContext")));
+//builder.Services.AddDbContext<ToDoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
