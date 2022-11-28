e474ac7b7fd5ec4f926e62180deced3bd30b5e85
ajwhite00 <aaronjwhite00@gmail.com>
2022-11-28 13:29:35 -0500

    updates to controller logic

--- WebApi/Controllers/StudentController.cs
+++ WebApi/Controllers/StudentController.cs
@@ -56,15 +56,15 @@ public class StudentController : ControllerBase
     public IActionResult DeleteStudent(int id)
     {
         try {
-            var Student = _context.Students?.Find(id);
-            if (Student == null) return NotFound($"Student with id {id} was not found");
+            var student = _context.Students?.Find(id);
+            if (student == null) return NotFound($"Student with id \"{id}\" was not found");
 
-            _context.Students?.Remove(Student);
+            _context.Students?.Remove(student);
             var result = _context.SaveChanges();
 
             if (result < 1) return Problem("Delete was not successful. Please try again");
 
-            return Ok("Deletion successful");
+            return Ok($"Successfully deleted student with id \"{id}\", Student Name: {student.Name}");
         }
         catch(Exception e){
                 return Problem(e.Message);
@@ -75,16 +75,16 @@ public class StudentController : ControllerBase
     [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
-    public IActionResult CreateStudent(Student StudentToAdd){
-        StudentToAdd.Id = 0;
+    public IActionResult CreateStudent(Student studentToAdd){
+        studentToAdd.Id = 0;
         try
         {
-            _context.Students?.Add(StudentToAdd);
+            _context.Students?.Add(studentToAdd);
             var result = _context.SaveChanges();
 
             if(result < 1) return Problem("Addition was not successful. Please try again");
 
-            return Ok("Addition successful");
+            return Ok($"Successfully added student. Name: {studentToAdd.Name}, Birthdate: {studentToAdd.BirthDate}, College Program: {studentToAdd.CollegeProgram}, Year in College: {studentToAdd.YearInCollege}.");
         }
         catch(Exception e)
         {
@@ -97,24 +97,24 @@ public class StudentController : ControllerBase
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
-    public IActionResult UpdateStudent(Student StudentToEdit){
-        if (StudentToEdit.Id < 1) return BadRequest("Pleas provide a valid Id");
+    public IActionResult UpdateStudent(Student studentToEdit){
+        if (studentToEdit.Id < 1) return BadRequest($"\"{studentToEdit.Id}\" is not a valid Id. Please use a valid Id.");
 
         try{
-            var Student = _context.Students?.Find(StudentToEdit.Id);
-            if (Student == null) return NotFound("Student was not found");
+            var student = _context.Students?.Find(studentToEdit.Id);
+            if (student == null) return NotFound($"Student with id \"{studentToEdit.Id}\" not found");
             
-            Student.Name = StudentToEdit.Name;
-            Student.BirthDate = StudentToEdit.BirthDate;
-            Student.CollegeProgram = StudentToEdit.CollegeProgram;
-            Student.YearInCollege = StudentToEdit.YearInCollege;
+            if (studentToEdit.Name != null) student.Name = studentToEdit.Name;
+            if (studentToEdit.BirthDate != DateTime.Parse("0001-01-01T00:00:00")) student.BirthDate = studentToEdit.BirthDate;
+            if (studentToEdit.CollegeProgram != null) student.CollegeProgram = studentToEdit.CollegeProgram;
+            if (studentToEdit.YearInCollege > 0) student.YearInCollege = studentToEdit.YearInCollege;
 
-            _context.Students?.Update(Student);
+            _context.Students?.Update(student);
             var result = _context.SaveChanges();
 
             if (result < 1 ) return Problem("Update was not successful. Please try again");
 
-            return Ok("Update successful");
+            return Ok($"Update for student with id \"{studentToEdit.Id}\" was successful");
         }
         catch(Exception e)
         {
