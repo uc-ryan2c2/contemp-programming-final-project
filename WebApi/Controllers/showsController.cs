9302663475279f887364e5ea9c2db06150bf139d
uc-ryan2c2 <ryan2c2@mail.uc.edu>
2022-11-29 12:10:50 -0500

    updating

--- WebApi/Controllers/showsController.cs
+++ WebApi/Controllers/showsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,34 +9,79 @@ namespace contemp_programming_final_project.Controllers
     [Route("api/shows/[controller]")]
     public class showsController : ControllerBase
     {
+        private readonly ILogger<showsController> _logger;
+        private readonly DataContext _context;
+
+        public showsController(ILogger<showsController> logger, DataContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
         // GET: api/<showsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        [ProducesResponseType(typeof(Shows), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<Shows>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult GetStudent(int? id)
+    {
+        if (id == null){
+            try
+            {
+                if (_context.Shows == null || !_context.Shows.Any()) return NotFound("No Students found in the database.");
+                return Ok(_context.Students?.Take(5).ToList());
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+        } 
+        else 
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var show = _context.Shows?.Find(id);
+                if (show == null) return NotFound($"Student with id {id} was not found.");
+                return Ok(show);
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
         }
-
-        // GET api/<showsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    }
 
         // POST api/<showsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateStudent(Student showToAdd)
+    {
+        showToAdd.Id = 0;
+        try
         {
+            _context.Students?.Add(showToAdd);
+            var result = _context.SaveChanges();
+
+            if(result < 1) return Problem("Addition was not successful. Please try again");
+
+            return Ok("Addition successful");
         }
+        catch(Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
 
         // PUT api/<showsController>/5
-        [HttpPut("{id}")]
+        [HttpPut]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/<showsController>/5
-        [HttpDelete("{id}")]
+        [HttpDelete]
         public void Delete(int id)
         {
         }
